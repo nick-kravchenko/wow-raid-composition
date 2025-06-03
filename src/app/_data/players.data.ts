f5af3a13da0bfb2b0f4d9b413a2c4ff4af21ff96
Mykola Kravchenko <krava9519@gmail.com>
2025-06-03 23:32:44 +0300

    update user data

--- src/app/_data/players.data.ts
+++ src/app/_data/players.data.ts
@@ -934,7 +934,14 @@ export const players: Player[] = [
         rank: CharacterRank.main,
         role: CharacterRole.healer,
         class: CharacterClass.priest,
-      }
+      },
+      {
+        name: 'Oversîr',
+        level: 60,
+        rank: CharacterRank.alt,
+        role: CharacterRole.melee,
+        class: CharacterClass.warrior,
+      },
     ]
   },
   {
