06673bb68a4209a7f74c0d639742e4c90a23d50a
Mykola Kravchenko <krava9519@gmail.com>
2026-07-27 09:07:54 +0300

    feat: enhance home page layout with new sections and footer for improved user experience

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
