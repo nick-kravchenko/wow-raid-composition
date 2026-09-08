af32a2f7d6ae8dbde51a9e8d1b6b5d7ad742de30
Mykola Kravchenko <krava9519@gmail.com>
2026-09-08 11:42:01 +0300

    fix(players): update Flinesham's role to melee and spec to Enhancement

--- src/app/_data/players.data.ts
+++ src/app/_data/players.data.ts
@@ -912,9 +912,9 @@ export const players: Player[] = [
       {
         name: 'Flinesham',
         rank: CharacterRank.alt,
-        role: CharacterRole.ranged,
+        role: CharacterRole.melee,
         class: CharacterClass.shaman,
-        spec: CharacterSpecEnum.Elemental,
+        spec: CharacterSpecEnum.Enhancement,
       },
     ],
   },
